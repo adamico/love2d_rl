fc9836dbdfe764626d5d9c37708821ff94fd76c1
Andrea D'Amico <andrea.damico@gmail.com>
2025-03-20 18:04:13 +0100

    removing all menu stuff

--- systems/buildMenuSystem.lua
+++ systems/buildMenuSystem.lua
@@ -1,38 +1,7 @@
-local helium = require("helium")
-
-MenuScene = helium.scene.new(true)
-ModalScene = helium.scene.new(true)
-
-ModalActive = false
-
-local function switchToModal()
-	ModalActive = true
-	GaussWeen = 1
-	MenuScene:deactivate()
-	ModalScene:activate()
-end
-
-local function switchToMenu()
-	ModalActive = false
-	GaussWeen = 0
-	ModalScene:deactivate()
-	MenuScene:activate()
-end
-
 local system = System()
 
 function system:load()
-    GaussWeen = 0
-    MenuScene:draw()
 
-    Modal = require("main-menu.modal")({switch = switchToMenu}, 400, 400)
-    Menu = require("main-menu.menu")({switch = switchToModal}, 200, 200)
-    ModalScene:activate()
-    Modal:draw(400, 200)
-    MenuScene:activate()
-    Menu:draw(0, 0, 1200, 800)
-
-    local fsc = love.graphics.newCanvas(1200/4, 800/4)
 end
 
 return system
--- systems/drawMenuSystem.lua
+++ systems/drawMenuSystem.lua
@@ -1,34 +1,11 @@
-local moonshine = require("moonshine")
-
-local gaussBlurs = {
-    moonshine(moonshine.effects.fastgaussianblur),
-    moonshine(moonshine.effects.fastgaussianblur),
-    moonshine(moonshine.effects.fastgaussianblur),
-    moonshine(moonshine.effects.fastgaussianblur),
-    moonshine(moonshine.effects.fastgaussianblur),
-}
-gaussBlurs[1].fastgaussianblur.taps = 11
-gaussBlurs[2].fastgaussianblur.taps = 15
-gaussBlurs[3].fastgaussianblur.taps = 21
-gaussBlurs[4].fastgaussianblur.taps = 37
-gaussBlurs[5].fastgaussianblur.taps = 51
-
 local system = System()
 
 function system:draw()
-    if ModalActive then
-		gaussBlurs[GausWeen](function()
-			MenuScene:draw()
-		end)
-		ModalScene:draw()
-	else
-		MenuScene:draw()
-	end
+  
 end
 
 function system:resize(w, h)
-    Menu:draw(0, 0, w, h)
-	MenuScene:resize(w, h)
+
 end
 
 return system
